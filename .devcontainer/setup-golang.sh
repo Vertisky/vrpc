6b28d83b958b25508f672fa07b4412756a582770
Erik 'ETMA' Andersen <etma@etma.dk>
2023-10-22 15:31:16 +0200

    move dev environment out of module repo

--- .devcontainer/setup-golang.sh
+++
@@ -1,7 +0,0 @@
-go install -v golang.org/x/tools/gopls@latest
-go install -v github.com/rogpeppe/godef@latest
-go install -v github.com/ramya-rao-a/go-outline@latest
-go install -v github.com/rogpeppe/godef@latest
-go install -v golang.org/x/tools/cmd/goimports@latest
-go install -v github.com/stamblerre/gocode@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
